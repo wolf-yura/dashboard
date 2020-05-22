9122535de1c725d111fab8f47664c0d4d71eb186
wolf-yura <baba.takeshi95@gmail.com>
2020-05-22 14:41:23 -0300

    make it better

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import authHeader from './auth-header';
-const URL = 'http://localhost:8080/api/user/';
+const URL = 'https://xcapitalsc.herokuapp.com/api/user/';
 
 class UserService {
   // getPublicContent() {
