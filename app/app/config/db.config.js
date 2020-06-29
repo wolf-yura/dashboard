387bb225287464102bbd788d3bd045226d25c55a
wolf-yura <baba.takeshi95@gmail.com>
2020-06-29 04:10:59 -0300

    translate text

--- app/app/config/db.config.js
+++ app/app/config/db.config.js
@@ -1,7 +1,7 @@
  module.exports = {
      HOST: "qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
      USER: "xhfa5u8xtozcyxub",
-     PASSWORD: "pfgh342fi1yii626",
+     PASSWORD: "p1t3rqy5248853kr",
      DB: "jsqwhof9f0zvw2op",
      dialect: "mysql",
      pool: {
